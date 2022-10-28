22ba62e3253f4baf44de9cecd6f9c7497a960856
devguiamf <guilherme.amf18@gmail.com>
2022-10-28 18:18:23 -0300

    feat: segundo commit

--- src/app/ingles/ingles.component.ts
+++ src/app/ingles/ingles.component.ts
@@ -1,5 +1,5 @@
 import { VidasComponent } from './../vidas/vidas.component';
-import { Component, Inject, inject, OnInit } from '@angular/core';
+import { Component, Inject, inject, OnInit, Output } from '@angular/core';
 import { Injectable } from '@angular/core';
 
 @Component({
@@ -7,25 +7,23 @@ import { Injectable } from '@angular/core';
   templateUrl: './ingles.component.html',
   styleUrls: ['./ingles.component.scss']
 })
-@Injectable({
-  providedIn: 'root'
-})
 
 export class InglesComponent implements OnInit {
-  changeVidas = 0
+  @Output() vidasPai: number = 3
 
-  constructor(
-    private vidas: VidasComponent
-    ) { }
+  constructor() { }
 
   ngOnInit(): void {
 
   }
 
-  async trocar(){
-
-    this.changeVidas = this.vidas.vidas = 3
-    this.vidas.vidas = 3
-    console.log(this.changeVidas)
+  trocar(){
+    const vida1 = 1
+    this.vidasPai -= -vida1 
+    if(this.vidasPai = 0){
+      console.log('vidas zeradas');
+      
+    }
   }
+
 }
--- src/app/vidas/vidas.component.ts
+++ src/app/vidas/vidas.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, Output, Input } from '@angular/core';
 import { Injectable } from '@angular/core';
 @Component({
   selector: 'app-vidas',
   templateUrl: './vidas.component.html',
   styleUrls: ['./vidas.component.scss']
 })
-@Injectable({
-  providedIn: 'root'
-})
+
 export class VidasComponent implements OnInit {
-  vidas = 0
-  constructor() { this.vidas}
+  
+  @Input() vidas:number = 3
+  constructor() {}
 
   ngOnInit(): void {
   }
